46239fcddb73377de7b85fd7b79e14760a5fcebf
Unknown <tkwan4@ucsc.edu>
2018-05-01 00:52:57 -0700

    add dropzone.js and implement temp solution for image preview

--- static/js/add_post.js
+++ static/js/add_post.js
@@ -38,13 +38,30 @@ var app = function() {
     delimiters: ['${', '}'],
     unsafeDelimiters: ['!{', '}'],
     data: {
+      imageData: "",
       message: null,
-      tag: null
+      tag: null,
+
     },
     methods: {
-
+      previewImage: function(event) {
+        // Reference to the DOM input element
+        var input = event.target;
+        // Ensure that you have a file before attempting to read it
+        if (input.files && input.files[0]) {
+          // create a new FileReader to read this image and convert to base64 format
+          var reader = new FileReader();
+          // Define a callback function to run, when FileReader finishes its job
+          reader.onload = (e) => {
+              // Note: arrow function used here, so that "this.imageData" refers to the imageData of Vue component
+              // Read image as base64 and set to imageData
+              this.imageData = e.target.result;
+            }
+            // Start the reader job - read file as a data url (base64 format)
+          reader.readAsDataURL(input.files[0]);
+        }
+      }
     }
-
   });
 
   $("#add-div").show();
