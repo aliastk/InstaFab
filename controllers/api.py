f79948921cdf2bef15d091684ec6a70868fb58fa
Janelynn Camingue <greentomboy@sbcglobal.net>
2018-05-18 00:10:06 -0700

    Added option to edit posts

--- controllers/api.py
+++ controllers/api.py
@@ -1,1 +1,15 @@
 # Here go your api methods.
+@auth.requires_login()
+def edit():
+    """
+    - "/edit/3" it offers a form to edit a checklist.
+    'edit' is the controller (this function)
+    '3' is request.args[0]
+    """
+
+    q = ((db.checklist.user_email == auth.user.email) &
+        (db.checklist.id == request.vars.id))
+    CurrentPost=db(q).select().first()
+    CurrentPost.update_record(title = request.vars.title , memo = request.vars.memo)
+    #redirect(URL('default', 'index'))
+    return "ok"
